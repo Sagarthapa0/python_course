00a828ad5007805718d0f53dfe3441984dda7967
Sagar Thapa <st163528@gmail.com>
2024-02-27 15:25:37 +0545

    this is basic

---
+++ basic.py
@@ -0,0 +1,15 @@
+# user_age=int(input("Enter your age:"))
+# if user_age>18:
+#     print("you can go")
+# elif user_age>35:
+#     print("dont go")
+# else:
+#     print("you cant go")
+    
+
+# radius=float(input("enter your radius"))
+# area=22/7*radius**2
+# print(f"the area is {area}")
+
+a=3
+print("distinction")
