6429ca052eed10ea347c52fc81298f1807b48894
Sagar Thapa <155831698+Sagarthapa0@users.noreply.github.com>
2024-03-23 18:04:08 +0545

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+Q.calculate area
+
+def function_name(args,args1):
+    print("task")
+
+def calculate_area(base,height):
+    return (1/2)*base*height
+
+base=1
+height=3
+area=calculate_area(base,height)
+print(f"the area is {area}")
+
+#'''
+#WAF that calculates volume of cube and cuboid.
+
+def calculate_volume(base,height,width):
+    return base*width*height
+base=4
+height=5
+width=9
+volume=calculate_volume(base,height,width)
+print(f"the volume is {volume}")
